4fd1a1ee1cd3285fa56d7fb8e65c4bf0d4aea2af
Dylan Varney <djvarney42@tntech.edu>
2023-11-23 22:07:36 -0600

    Made it possible to change around players that are currently on the court from a list. Also made buttons recognize *which* button was being pressed and *with which* player they were selected. Also made players a click-and-stick model.

--- wbb-analytics/src/pages/Drill/ButtonExplanation.js
+++ wbb-analytics/src/pages/Drill/ButtonExplanation.js
@@ -3,16 +3,20 @@
 import React, { useState } from 'react';
 import './DrillPage.css';
 
-const ButtonExplanation = ({ isOpen, onClose, whichButton }) => {
-  const [drillName, setDrillName] = useState('');
-  const [drillType, setDrillType] = useState('');
-  const [paraBody, setParaBody] = useState("");
+const ButtonExplanation = ({ isOpen, onClose, whichButton, whichPlayer}) => {
 
-  const updateParagraph () => {
+  const updateParagraph = () => { //You have to *call* the function within the HTML in order to get it to change what it gives
     switch(whichButton){
       case "Rebound":
-        setParaBody("The Rebound button is pressed when a player has created a rebound, and adds 1 to the session's rebound count.");
-        break;
+        return <p>The Rebound button is pressed when {whichPlayer} has created a rebound, and adds 1 to the session's rebound count.</p>;
+      case "Steal":
+        return <p>The Steal button is pressed when {whichPlayer} performs a steal, and adds 1 to the session's and the selected player's steal count.</p>
+      case "Turnover":
+        return <p>The Turnover Button is pressed when {whichPlayer} performs a Turnover, and adds 1 to the session's Turnover count.</p>
+      case "Assist":
+        return <p>The Assist button is pressed when {whichPlayer} performs an assist, and adds 1 to the player's and session's Assist count.</p>
+      default:
+        return <p>How did you get here?</p>
     }
   }
   
@@ -21,7 +25,7 @@ const ButtonExplanation = ({ isOpen, onClose, whichButton }) => {
       <div className="modal-overlay">
         <div className="modal-content">
           <h2>The {whichButton} Button</h2>
-          <p>{paraBody}</p>         
+          {updateParagraph()}         
           <button onClick={onClose}>Close</button>
         </div>
       </div>
--- wbb-analytics/src/pages/Drill/DrillPage.js
+++ wbb-analytics/src/pages/Drill/DrillPage.js
@@ -7,7 +7,11 @@ import './DrillPage.css';
 const CreateSessionsPage = () => {
   const [drills, setDrills] = useState([]);
   const [isModalOpen, setModalOpen] = useState(false);
+
   const [whichButtonPressed, setButtonPress] = useState("");
+  const [selectedPlayer, setSelectedPlayer] = useState(null);
+  const [availablePlayers, setAvailablePlayers] = useState([]);
+
   const [sessionName, setSessionName] = useState('');
   const [savedSessions, setSavedSessions] = useState([]);
 
@@ -25,23 +29,29 @@ const CreateSessionsPage = () => {
   const playerArray = useMemo(
     () => [
       'Player 1', 'Player 2', 'Player 3', 'Player 4', 'Player 5',
-      'Player B', 'Player A', 'Player C', 'Player D', 'Player E'
+      'Player A', 'Player B', 'Player C', 'Player D', 'Player E'
     ],
     []
   );
 
   useEffect(() => {
     // Populate default selections for List A
-    const defaultListA = Array.from({ length: 5 }, (_, index) => ({
+    const defaultListA = Array.from({ length: 7 }, (_, index) => ({
       playerName: playerArray[index],
     }));
     setListA(defaultListA);
+    setAvailablePlayers(prevAvailablePlayers => {
+      const listASlice = listA.slice(5).map(item => item.playerName);
+      return [...prevAvailablePlayers, ...listASlice];
+    });
 
     // Populate default selections for List B
     const defaultListB = Array.from({ length: 5 }, (_, index) => ({
       playerName: playerArray[5 + index],
     }));
     setListB(defaultListB);
+
+    
   }, [playerArray]);
 
   const handleAddDropdownA = () => {
@@ -71,21 +81,53 @@ const CreateSessionsPage = () => {
     setListB([]);
   };
 
+  const handlePlayerChange = (index, newPlayer) => {
+    setAvailablePlayers(prevAvailablePlayers => {
+      // If the replaced player exists, add it back to available players
+      const replacedPlayer = listA[index]?.playerName;
+      if (replacedPlayer) {
+        return [...prevAvailablePlayers, replacedPlayer];
+      }
+      return prevAvailablePlayers;
+    });
+  
+    setListA(prevListA => {
+      const newList = [...prevListA];
+      newList[index] = { playerName: newPlayer };
+      return newList;
+    });
+
+    setListA(prevListB => {
+      const newList = [...prevListB];
+      newList[index] = { playerName: newPlayer };
+      return newList;
+    });
+  
+    setAvailablePlayers(prevAvailablePlayers => {
+      // Exclude the newly selected player from available players
+      return prevAvailablePlayers.filter(player => player !== newPlayer);
+    });
+  };
+
   return (
     <div className="drills-container">
       <div className="teams-court-container">
         <div className="teams-column">
           <h2>Team Roster</h2>
           <ul>
-            {listA.map((item, index) => (
-              <li key={index}>
-                <select className='dropdown'>
-                  {playerArray.map((player, playerIndex) => (
-                    <option key={playerIndex} value={player}>
-                      {player}
-                    </option>
-                  ))}
-                </select>
+            {listA.slice(0,5).map((item, index) => (  
+              <li key={index}> {/* The array in the line above is sliced such that if a team has more than five players assigned, the drill will automatically only show the first five. */}
+                <button key={index} className={selectedPlayer === item ? 'selectPlayer' : 'selectPlayer'} onClick={() => setSelectedPlayer(item.playerName)}>
+                  {item.playerName}
+                </button>
+                <select className='changePlayer'
+                value={item.playerName || ''}
+                onChange={(e) => handlePlayerChange(index, e.target.value)}>
+                <option value="" hidden>Select Player</option>
+                {availablePlayers.map((player, idx) => (
+                  <option key={idx} value={player}>{player}</option>
+                ))}
+              </select>
               </li>
             ))}
           </ul>
@@ -100,12 +142,12 @@ const CreateSessionsPage = () => {
       </div>
 
       <div className="buttons-container">
-        <button className="button-rebound" onClick={()=>{setModalOpen(true); setButtonPress("Rebound")}}>Rebound</button> {/*REACT/JS DOES NOT LIKE &&*/}
-        <button className="button-steal">Steal</button>
-        <button className="button-turnover">Turnover</button>
-        <button className="button-assist">Assist</button>
-        <button className="button-block">Block</button>
-        <ButtonExplanation isOpen={isModalOpen} onClose={() => setModalOpen(false)} whichButton={whichButtonPressed} />
+        <button className={`button-rebound ${selectedPlayer ? '' : 'greyed-out'}`} onClick={()=>{setModalOpen(true); setButtonPress("Rebound")}}>Rebound</button> {/*REACT/JS DOES NOT LIKE &&*/}
+        <button className={`button-steal ${selectedPlayer ? '' : 'greyed-out'}`} onClick={()=>{setModalOpen(true); setButtonPress("Steal")}}>Steal</button>
+        <button className={`button-turnover ${selectedPlayer ? '' : 'greyed-out'}`} onClick={()=>{setModalOpen(true); setButtonPress("Turnover")}}>Turnover</button>
+        <button className={`button-assist ${selectedPlayer ? '' : 'greyed-out'}`} onClick={()=>{setModalOpen(true); setButtonPress("Assist")}}>Assist</button>
+        <button className={`button-block ${selectedPlayer ? '' : 'greyed-out'}`} onClick={()=>{setModalOpen(true); setButtonPress("Block")}}>Block</button>
+        <ButtonExplanation isOpen={isModalOpen} onClose={() => {setModalOpen(false); setSelectedPlayer(null)}} whichButton={whichButtonPressed} whichPlayer={selectedPlayer}/>
       </div>
 
     </div>
