be80a1c7a36a6bfc75f4f66dc6cb7d8505e8cacf
Dylan Varney <djvarney42@tntech.edu>
2023-11-24 22:31:40 -0600

    Enabled the user to switch between teams and the players that are either on or off the court. Also added several javadoc comments to clarify the important functions that run the page.

--- wbb-analytics/src/pages/Drill/ButtonExplanation.js
+++ wbb-analytics/src/pages/Drill/ButtonExplanation.js
@@ -5,6 +5,11 @@ import './DrillPage.css';
 
 const ButtonExplanation = ({ isOpen, onClose, whichButton, whichPlayer}) => {
 
+  /**
+   * Function that takes the value of "whichButton" and changes the body paragraph of the pop-up returned by its parent function based on that value.
+   * 
+   * @returns   A paragraph bloc based on the value of the whichButton variable.
+   */
   const updateParagraph = () => { //You have to *call* the function within the HTML in order to get it to change what it gives
     switch(whichButton){
       case "Rebound":
@@ -15,6 +20,12 @@ const ButtonExplanation = ({ isOpen, onClose, whichButton, whichPlayer}) => {
         return <p>The Turnover Button is pressed when {whichPlayer} performs a Turnover, and adds 1 to the session's Turnover count.</p>
       case "Assist":
         return <p>The Assist button is pressed when {whichPlayer} performs an assist, and adds 1 to the player's and session's Assist count.</p>
+      case "Block":
+        return <p>The Block button is pressed when {whichPlayer} performs a block, and adds 1 to the player's and session's Block count.</p>
+      case "Start Tempo":
+        return <p>The Start Tempo button is pressed when the user wants to record the beginning of a tempo stat.</p>
+      case "Stop Tempo":
+          return <p>The Stop Tempo button is pressed when the user wants to stop recording the tempo, after the Start Tempo button has been pressed.</p>
       default:
         return <p>How did you get here?</p>
     }
