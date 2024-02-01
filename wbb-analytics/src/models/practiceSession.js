f0ce1fe45421f2d189e99f193e74b9357be8c3b0
khermansk42 <khermansk42@tntech.edu>
2024-02-01 08:35:51 -0600

    added functionality to get player names from database

--- wbb-analytics/src/models/practiceSession.js
+++ wbb-analytics/src/models/practiceSession.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 const practiceSessionSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
+    Name: String,
     Date: String, // Assuming 'Date' is a string based on your JSON. Adjust if it's an actual Date.
-    DrillIDs: mongoose.Schema.Types.ObjectId // Assuming this refers to a single drill ID.
+    DrillIDs: [mongoose.Schema.Types.ObjectId] // Assuming this refers to a single drill ID.
 });
 
 module.exports = mongoose.model('PracticeSession', practiceSessionSchema);
