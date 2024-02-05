0bf89ac54af24c7b700cedc80885f92c1a55d01e
Brandon Parton <brparton42@tntech.edu>
2024-02-05 12:47:49 -0600

    Revert Hash "db396c18040f864ef8fe2af3465377472e4540ae"

--- wbb-analytics/src/App.js
+++ wbb-analytics/src/App.js
@@ -1,24 +1,12 @@
 // App.js
 import React from 'react';
+
 import Tempo from './pages/Tempo/Tempo'
-import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/Home/HomePage';
-import PlayersPage from './pages/Stats/PlayersPage';
-import TeamStatsPage from './pages/TeamStats/TeamStatsPage';
-import CreateSessionsPage from './pages/CreateSession/CreateSessionPage';
-import DrillPage from './pages/Drill/DrillPage';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/players" element={<PlayersPage />} />
-        <Route path="/teamstats" element={<TeamStatsPage />} />
-        <Route path="/createsession" element={<CreateSessionsPage />} />
-        <Route path="/drill" element={<DrillPage/>}/>
-        <Route path='/tempo' element={<Tempo />} />
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <Tempo /> {/* Render your TempoPage component here */}
     </div>
   );
 }
