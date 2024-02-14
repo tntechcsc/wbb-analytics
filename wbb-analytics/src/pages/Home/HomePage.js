10e5043a7f9af66ac16afe23518658f8f6a7543f
Matthew Sims <mjsims42@tntech.edu>
2024-02-14 11:28:16 -0600

    Change visuals of Homepage

--- wbb-analytics/src/pages/Home/HomePage.js
+++ wbb-analytics/src/pages/Home/HomePage.js
@@ -4,43 +4,21 @@ import React,{useState} from 'react';
 import { Link } from 'react-router-dom';
 import SessionOption from './SessionOption.js';
 import './HomePage.css';
-
-
+import logo from '../../images/nESTlogo.png';
 
 const HomePage = () => {
   const [isModelOpen, setModelOpen] = useState(false);
-
+  
   return (
     <div className="home-page-container">
-      <header>
-        <h1>WBB Analytics</h1>
-        <p>Your Hub for Women's Basketball Statistics</p>
-      </header>
-
-      
-
-      <section className="new-session">
-        <h2>Create a New Practice Session</h2>
-        <p>Start a new practice session to track player progress and team performance.</p>
-        <button onClick={() => setModelOpen(true)} className="Linkish-Button">
+        <button onClick={() => setModelOpen(true)} className="Linkish-Button1">
           New Session
         </button>
-      </section>
-
-      <section className="open-session">
-        <h2>Explore Stats</h2>
-        <p>View details of women's basketball team</p>
-        <button className="Linkish-Button">
+        <button className="Linkish-Button2">
           Stats
         </button>
-      </section>
 
       <section className="about">
-        <h2>About WBB Analytics</h2>
-        <p>
-          WBB Analytics provides comprehensive insights into women's basketball player performance,
-          empowering coaches and enthusiasts with valuable statistical information.
-        </p>
       </section>
       <SessionOption isOpen={isModelOpen} onClose={() => setModelOpen(false)}/>
     </div>
