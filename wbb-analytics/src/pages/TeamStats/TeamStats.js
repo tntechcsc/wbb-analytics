f654ca473f9ce5eda5f6afb96fc477498ad5c8e2
Brandon Parton <ssphenic@gmail.com>
2024-02-20 12:44:18 -0600

    Load sessions and drills from database

--- wbb-analytics/src/pages/TeamStats/TeamStats.js
+++ wbb-analytics/src/pages/TeamStats/TeamStats.js
@@ -1,157 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Selector from './components/Selector';
 import Heatmap from './components/Heatmap';
 import TempoCard from './components/TempoCard';
 import StatCard from './components/StatsDisplay';
 import StatsTable from './components/StatsTable';
-import NavagationHeader from './components/NavagationHeader'
+import NavagationHeader from './components/NavagationHeader';
 import './TeamStats.css';
 
 function TeamStats() {
-  // Updated mock data to reflect the hierarchical relationship
-  const data = {
-    seasons: [
-      {
-        value: '2021',
-        label: '2021-2022',
-        sessions: [
-          {
-            value: '1',
-            label: 'Session Date: 10/15/2021',
-            drills: [
-              { value: 'shooting', label: 'Shooting' },
-              { value: 'defense', label: 'Defense' },
-            ],
-          },
-          {
-            value: '2',
-            label: 'Session Date: 10/20/2021',
-            drills: [
-              { value: 'offense', label: 'Offense' },
-              { value: 'freeThrows', label: 'Free Throws' },
-            ],
-          },
-          {
-            value: '3',
-            label: 'Session Date: 10/25/2021',
-            drills: [
-              { value: 'rebounding', label: 'Rebounding' },
-              { value: 'conditioning', label: 'Conditioning' },
-            ],
-          },
-          {
-            value: '4',
-            label: 'Session Date: 10/30/2021',
-            drills: [
-              { value: 'scrimmage', label: 'Scrimmage' },
-              { value: 'ballHandling', label: 'Ball Handling' },
-            ],
-          }
-        ],
-      },
-      {
-        value: '2022',
-        label: '2022-2023',
-        sessions: [
-          {
-            value: '1',
-            label: 'Session Date: 10/15/2022',
-            drills: [
-              { value: 'shooting', label: 'Shooting' },
-              { value: 'defense', label: 'Defense' },
-            ],
-          },
-          {
-            value: '2',
-            label: 'Session Date: 10/20/2022',
-            drills: [
-              { value: 'offense', label: 'Offense' },
-              { value: 'freeThrows', label: 'Free Throws' },
-            ],
-          },
-          {
-            value: '3',
-            label: 'Session Date: 10/25/2022',
-            drills: [
-              { value: 'rebounding', label: 'Rebounding' },
-              { value: 'conditioning', label: 'Conditioning' },
-            ],
-          },
-          {
-            value: '4',
-            label: 'Session Date: 10/30/2022',
-            drills: [
-              { value: 'scrimmage', label: 'Scrimmage' },
-              { value: 'ballHandling', label: 'Ball Handling' },
-            ],
-          },
-          {
-            value: '5',
-            label: 'Session Date: 11/05/2022',
-            drills: [
-              { value: '3ptShooting', label: '3-Point Shooting' },
-              { value: 'fastBreak', label: 'Fast Break' },
-            ],
-          }
-        ],
-      },
-    ],
-    selectedSeason: null,
-    selectedSession: null,
-    selectedDrill: null,
-    // Other stats data would go here
-  };
+  const [sessions, setSessions] = useState([]);
+  const [allDrills, setAllDrills] = useState([]);
+  const [filteredDrills, setFilteredDrills] = useState([]);
+  const [selectedSession, setSelectedSession] = useState('');
 
-  const statCardsData = [
-    { title: "3 Pt FG %", value: "43.2" }, // Points per game
-    { title: "2 Pt FG %", value: "77.5" },  // Assists per game
-    { title: "Rebounds", value: "50.1" },  // Rebounds per game
-    { title: "Steals", value: "8.2" },   // Steals per game
-  ];
+  useEffect(() => {
+    fetch('http://localhost:3001/api/sessions')
+      .then(response => response.json())
+      .then(data => {
+        const formattedSessions = data.map(session => ({
+          label: `Session: ${session.Date}`,
+          value: session._id.toString(), // Convert to string for consistency
+        }));
+        setSessions(formattedSessions);
 
-  const teamStatsData = {
-    headers: ["GP", "MPG", "PPG", "FGM", "FGA", "FG%", "3PM", "3PA", "3P%", "ORB", "DRB", "RPG", "APG", "SPG", "BPG", "TOV", "PF"],
-    rows: [
-      ["56", "48.0", "123.7", "46.8", "92.2", ".508", "13.6", "35.8", ".381", "10.0", "30.6", "40.6", "30.9", "7.8", "5.8", "13.4", "21.9"],
-      // ... Add as many rows as needed
-    ]
-  };
+        const urlParams = new URLSearchParams(window.location.search);
+        const sessionIdParam = urlParams.get('sessionId');
 
-  const [selectedData, setSelectedData] = useState({
-    selectedSeason: '',
-    selectedSession: '',
-    selectedDrill: '',
-  });
+        if (sessionIdParam && data.some(session => session._id.toString() === sessionIdParam)) {
+          setSelectedSession(sessionIdParam);
+        }
+      })
+      .catch(error => {
+        console.error('Failed to fetch sessions:', error);
+      });
 
-  // Handle selection changes and update state accordingly
-  const handleSeasonChange = (event) => {
-    setSelectedData({
-      ...selectedData,
-      selectedSeason: event.target.value,
-      selectedSession: '',
-      selectedDrill: '',
-    });
-  };
+    fetch('http://localhost:3001/api/drills')
+      .then(response => response.json())
+      .then(data => {
+        setAllDrills(data); // Assuming drills do not need to be formatted
+      })
+      .catch(error => {
+        console.error('Failed to fetch drills:', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    const filtered = allDrills.filter(drill => drill.SessionID === selectedSession);
+    const formattedDrills = filtered.map(drill => ({
+      label: `${drill.StartTime} - ${drill.EndTime}`,
+      value: drill._id,
+    }));
+    setFilteredDrills(formattedDrills);
+  }, [selectedSession, allDrills]);
 
   const handleSessionChange = (event) => {
-    setSelectedData({
-      ...selectedData,
-      selectedSession: event.target.value,
-      selectedDrill: '',
-    });
+    setSelectedSession(event.target.value);
   };
-
-  const handleDrillChange = (event) => {
-    setSelectedData({
-      ...selectedData,
-      selectedDrill: event.target.value,
-    });
-  };
-
-  // Filter sessions based on selected season
-  const sessionsOptions = data.seasons.find(season => season.value === selectedData.selectedSeason)?.sessions || [];
-  // Filter drills based on selected session
-  const drillsOptions = sessionsOptions.find(session => session.value === selectedData.selectedSession)?.drills || [];
 
   return (
     <div className="team-stats-container">
@@ -160,9 +64,19 @@ function TeamStats() {
       </header>
 
       <div className="selectors">
-        <Selector options={data.seasons} onChange={handleSeasonChange} label="Season" />
-        <Selector options={sessionsOptions} onChange={handleSessionChange} label="Session" disabled={!selectedData.selectedSeason} />
-        <Selector options={drillsOptions} onChange={handleDrillChange} label="Drill" disabled={!selectedData.selectedSession} />
+        <Selector
+          options={sessions}
+          onChange={handleSessionChange}
+          label="Session"
+          value={selectedSession}
+        />
+        <Selector
+          options={filteredDrills}
+          onChange={() => { }}
+          label="Drill"
+          value=""
+          disabled={!selectedSession} // Enable drill selector only when a session is selected
+        />
       </div>
 
       <div className="detailed-stats">
@@ -176,18 +90,17 @@ function TeamStats() {
       </div>
 
       <div className="stats-overview">
-        {statCardsData.map((card, index) => (
-          <StatCard key={index} title={card.title} value={card.value} />
-        ))}
+        <StatCard title="Total Points" value="78" />
+        <StatCard title="Total Rebounds" value="32" />
+        <StatCard title="Total Assists" value="15" />
       </div>
 
       <div className='stats-table-container'>
-      <h2>Team Stats</h2>
+        <h2>Team Stats</h2>
         <div className="stats-table">
-        <StatsTable stats={teamStatsData} />
+          {/* StatsTable component might go here */}
         </div>
       </div>
-        
     </div>
   );
 }
