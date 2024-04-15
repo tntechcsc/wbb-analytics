05d9aa24aac93a9e31a422accedf115eccde755f
Kenny VanGemert <kavangemer42@tntech.edu>
2024-04-15 17:32:56 -0500

    added made/missed & shotclock, still needs players and db posting

--- client/src/pages/Game/Game.js
+++ client/src/pages/Game/Game.js
@@ -4,6 +4,7 @@ import TempoTimer from '../Drill/components/TempoTimer';
 import TempoButton from '../Drill/components/TempoButton';
 import LastTempoDisplay from '../Drill/components/LastTempoDisplay';
 import CancelButton from '../Drill/components/CancelButton';
+import ShotPopup from '../Drill/components/ShotPopup';
 
 const Game = () => {
     const [opponentTeamInput, setOpponentTeamInput] = useState('');
@@ -14,9 +15,24 @@ const Game = () => {
     const [recordedTempo, setRecordedTempo] = useState(null);
     const [lastTempo, setLastTempo] = useState(null);
     const [tempoType, setTempoType] = useState(null);
-    const [isOpponentTeamInputVisible, setIsOpponentTeamInputVisible] = useState(true);
     const [isOpponentTeamOverlayVisible, setIsOpponentTeamOverlayVisible] = useState(true);
+    const [shotOutcome, setShotOutcome] = useState(null);
+    const [isSubmitClicked, setIsSubmitClicked] = useState(false);
     const serverUrl = process.env.REACT_APP_SERVER_URL;
+
+    useEffect(() => {
+        setIsOpponentTeamOverlayVisible(false);
+    }, []);
+
+    const handleOpponentTeamSubmit = () => {
+        setIsSubmitClicked(true);
+        console.log('Opponent Team:', opponentTeamInput);
+        console.log('Location:', locationInput);
+    };
+    
+    const handleLocationClick = (location) => {
+        setLocationInput(location);
+    };
 
     const startTempo = (tempoType) => {
         setIsTiming(true);
@@ -38,33 +54,108 @@ const Game = () => {
         setResetTimer(true);
     };
 
-    // Focus on the input when the overlay is visible
-    useEffect(() => {
-        if (isOpponentTeamOverlayVisible) {
-            document.getElementById("opponent-team-input").focus();
+    const handleClockTimeSelection = (timeMapping) => {
+        if (shotOutcome) {
+            submitGame(shotOutcome === 'made', timeMapping);
+            console.log('Shot submitted:', shotOutcome, timeMapping);
+            setShotOutcome(null); // Reset shot outcome
         }
-    }, [isOpponentTeamOverlayVisible]);
-
-
-    const handleOpponentTeamSubmit = () => {
-        setIsOpponentTeamInputVisible(false); // Hide the opponent team input
-        setIsOpponentTeamOverlayVisible(false); // Hide the overlay
-
-        console.log('Opponent Team:', opponentTeamInput);
-        console.log('Location:', locationInput);
+    };
+    
+    const handleShotOutcome = (outcome) => {
+        setShotOutcome(outcome);
     };
 
-    const handleLocationClick = (location) => {
-        setLocationInput(location);
-    };
+    /*
+        post a shot with:
+        gameOrDrill_id: game_id
+        onModel: "Game"
+        player_ids: id of player who took the shot
+        made: true / false
+        shot_clock_time: selected shot clock time
+        timestamp: time player took shot
 
-    const currentDate = new Date();
-    const newDate = `${currentDate.getFullYear()}-${(currentDate.getMonth() + 1).toString().padStart(2, '0')}-${currentDate.getDate().toString().padStart(2, '0')}`;
+        after shot is posted, we get an obj id -> (data._id probably), w that shot
+        and append that shot to an array of shot_ids for shot_events
+
+        post to game with:
+        date: date
+        opponent: opponent team name
+        location: selected location
+        tempo_events: all recorded tempos
+        shot_events: all recorded shot_ids
+
+        submitGame is a partial mock submission to bypass errors for now, it will be updated 
+        later with the above information as everything is added and ready
+
+        shots need to be tied to a player so players still need to be implemented in this page,
+        also change tempo_events to be an array of all tempos taken, add tempo to array each time
+    */
+
+    const submitGame = (shotClockTime) => {
+        const currentDate = new Date();
+        const date = `${currentDate.getFullYear()}-${(currentDate.getMonth() + 1).toString().padStart(2, '0')}-${currentDate.getDate().toString().padStart(2, '0')}`;
+
+        const gameData = {
+            date: date,
+            opponent: opponentTeamInput,
+            location: locationInput,
+            tempo_events: recordedTempo,
+            shot_events: shotClockTime,
+        };
+        console.log('Submitting game data:', gameData);
+
+        fetch(serverUrl + '/api/game', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(gameData)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Game data submitted:', data);
+        })
+        .catch(error => console.error('Error submitting shot:', error));
+    };
 
     return (
         <>
+            {!isOpponentTeamOverlayVisible && !isSubmitClicked && (
+                <div className="overlay">
+                    <div className="overlay-content">
+                        <h3> Opponent Team Name </h3>
+                        <input
+                            id="opponent-team-input"
+                            type="text"
+                            value={opponentTeamInput}
+                            onChange={(e) => setOpponentTeamInput(e.target.value)}
+                        />                        
+                        <h3> Location </h3>
+
+                        <button
+                            onClick={() => handleLocationClick('home')}
+                            className={locationInput === 'home' ? '' : 'disabled'}
+                            disabled={locationInput === 'home'}
+                        >Home
+                        </button>
+
+                        <button
+                            onClick={() => handleLocationClick('away')}
+                            className={locationInput === 'away' ? '' : 'disabled'}
+                            disabled={locationInput === 'away'}
+                        >Away
+                        </button>
+
+                        <div className='submit-button'>
+                            <button onClick={handleOpponentTeamSubmit}> Submit </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+
             <div className="main">
-                <h1> Game Tempo </h1>
+                <h1> Game Mode </h1>
                 
                 <div className='tempo-timer'>
                     <TempoTimer 
@@ -104,35 +195,28 @@ const Game = () => {
                     <CancelButton onCancel={cancelTempo} />
                 </div>
 
-            </div>
-
-            {isOpponentTeamOverlayVisible && (
-                <div className="overlay">
-                    <div className="overlay-content">
-                        <h3> Opponent Team Name </h3>
-                        <input id="opponent-team-input" type="text" value={opponentTeamInput} onChange={(e) => setOpponentTeamInput(e.target.value)} />
-                        
-                        <h3> Location </h3>
-                        <button
-                            onClick={() => handleLocationClick('home')}
-                            className={locationInput === 'home' ? '' : 'disabled'}
-                            disabled={locationInput === 'home'}
-                        >Home
-                        </button>
-
-                        <button
-                            onClick={() => handleLocationClick('away')}
-                            className={locationInput === 'away' ? '' : 'disabled'}
-                            disabled={locationInput === 'away'}
-                        >Away
-                        </button>
-
-                        <div className='submit-button'>
-                            <button onClick={handleOpponentTeamSubmit}> Submit </button>
-                        </div>
+                <div className="ShotPopup">
+                    <ShotPopup
+                        isOpen={isOpponentTeamOverlayVisible}
+                        onClose={() => setIsOpponentTeamOverlayVisible(false)}
+                    />
+                    <div className="ShotOutcomeSelection">
+                        {!shotOutcome ? (
+                            <>
+                                <div className="MadeButton" onClick={() => handleShotOutcome('made')}>Made</div>
+                                <div className="MissedButton" onClick={() => handleShotOutcome('missed')}>Missed</div>
+                            </>
+                        ) : (
+                            <div className="ClockTimeSelection">
+                                <div className="ClockButton1" onClick={() => handleClockTimeSelection('first_third')}>1-10</div>
+                                <div className="ClockButton2" onClick={() => handleClockTimeSelection('second_third')}>11-20</div>
+                                <div className="ClockButton3" onClick={() => handleClockTimeSelection('final_third')}>21-30</div>
+                            </div>
+                        )}
                     </div>
                 </div>
-            )}
+
+            </div>
         </>
     );
 }
