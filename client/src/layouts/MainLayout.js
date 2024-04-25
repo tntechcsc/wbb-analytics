bfb026b49ba94d144c2f405dec68556152771663
khermansk42 <khermansk42@tntech.edu>
2024-04-24 22:41:46 -0500

    drillID change
    drillID in url changes according to which drill is selected.

--- client/src/layouts/MainLayout.js
+++ client/src/layouts/MainLayout.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Outlet, useLocation, useSearchParams } from 'react-router-dom';
+import { Outlet, useLocation, useSearchParams, useNavigate } from 'react-router-dom';
 import HamburgerMenu from '../components/HamburgerMenu/HamburgerMenu.js';
 import './MainLayout.css';
 
 const MainLayout = () => {
   const location = useLocation();
-  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
   const [practice, setPractice] = useState(null);
   const isDrillPage = location.pathname.includes('/drill');
   const serverUrl = process.env.REACT_APP_SERVER_URL;
@@ -18,26 +19,29 @@ const MainLayout = () => {
         .then(data => setPractice(data))
         .catch(error => console.error('Failed to fetch practice data:', error));
     }
-  }, [searchParams, isDrillPage]);
+  }, [searchParams, isDrillPage, serverUrl]);
 
   const handleDrillChange = (event) => {
-    console.log('Selected Drill:', event.target.value);
-    // Implement navigation to drill detail page if necessary
+    const newDrillID = event.target.value;
+    console.log('Selected Drill:', newDrillID);
+    // Update the URL with the new DrillID
+    navigate(`${location.pathname}?PracticeID=${searchParams.get('PracticeID')}&DrillID=${newDrillID}`, { replace: true });
   };
 
   const renderDrillOptions = () => {
     if (!practice) return <div>Loading or no practice data available...</div>;
+    const currentDrillID = searchParams.get('DrillID');
     return (
         <div className="center-group">
             <div className="practice-date">Date: {new Date(practice.date).toLocaleDateString()}</div>
-            <select className="drill-select" onChange={handleDrillChange}>
+            <select className="drill-select" value={currentDrillID} onChange={handleDrillChange}>
                 {practice.drills.map(drill => (
                     <option key={drill._id} value={drill._id}>{drill.name}</option>
                 ))}
             </select>
         </div>
     );
-};
+  };
 
   return (
     <div className="main-layout">
