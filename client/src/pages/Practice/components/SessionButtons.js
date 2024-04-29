55ee4f1af43a53c8b831e4829e07fdea32336123
Brandon Parton <99562075+brandonparton@users.noreply.github.com>
2024-04-29 13:05:05 -0500

    Final Fixes - Commit 1

--- client/src/pages/Practice/components/SessionButtons.js
+++ client/src/pages/Practice/components/SessionButtons.js
@@ -2,31 +2,26 @@ import React, { useState } from 'react';
 import { FaCalendarAlt } from 'react-icons/fa';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-
+ 
 const SessionButtons = ({ setDate }) => {
     const [startDate, setStartDate] = useState(new Date());
-
-    const handleAddSessionInfo = () => {
-        setDate(startDate.toISOString().substring(0, 10)); // Format as YYYY-MM-DD
-    };
-
+ 
     return (
         <>
             <div className='input-field'>
                 <FaCalendarAlt className="input-icon" />
                 <DatePicker
                     selected={startDate}
-                    onChange={(date) => setStartDate(date)}
+                    onChange={(date) => {
+                        setStartDate(date);
+                        setDate(date.toISOString().substring(0, 10)); // Set the date immediately when changed
+                    }}
                     dateFormat="MMMM d, yyyy"
                     className="datepicker"
                 />
             </div>
-
-            <button className='add-button' onClick={handleAddSessionInfo}>
-                Set Date
-            </button>
         </>
     );
 };
-
+ 
 export default SessionButtons;
